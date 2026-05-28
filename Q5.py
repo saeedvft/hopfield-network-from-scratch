713c9149a5bfa1b6964135423e3eb01f6ea4813a
saeednourian82 <saeednourian82@gmail.com>
2026-05-28 23:11:56 +0330

    feat: Implement prediction function

--- Q5.py
+++ Q5.py
@@ -16,7 +16,27 @@ class HopfieldedNetwork:
 
         np.fill_diagonal(self.weights, 0)
 
-        print(self.weights)
+        # print(self.weights)
+
+    def predict(self, pattern, max_iterations=100, async_update=False):
+        """Recall pattern using the update rule: s_i(t+1) = sign(Σ w_ij s_j(t))"""
+        pattern = pattern.copy()
+        prev_pattern = pattern.copy()
+        
+        for _ in range(max_iterations):
+            if async_update:
+                for i in range(self.n):
+                    local_field = np.dot(self.weights[i], pattern)
+                    pattern[i] = 1 if local_field >= 0 else -1
+            else:
+                local_fields = np.dot(self.weights, pattern)
+                pattern = np.where(local_fields >= 0, 1, -1)
+            
+            if np.array_equal(pattern, prev_pattern):
+                break
+            prev_pattern = pattern.copy()
+        
+        return pattern
 
 
     
@@ -28,3 +48,7 @@ if __name__ == "__main__":
 
     net = HopfieldedNetwork(6)
     net.train([p1, p2])
+
+    pred = net.predict([1, 1, 1, -1, -1, 1])
+
+    print(pred)
