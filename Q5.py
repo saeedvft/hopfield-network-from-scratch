25dc051d8e3b88b686147e158190c82e8f11b660
saeednourian82 <saeednourian82@gmail.com>
2026-05-28 23:10:04 +0330

    fix: Syntax error and minor fixes

--- Q5.py
+++ Q5.py
@@ -1,20 +1,30 @@
 import numpy as np
 
-class HopfieldedNetwork(self, n_neurons):
+class HopfieldedNetwork:
     def __init__(self, n_neurons):
         self.n = n_neurons
         self.weights = np.zeros((n_neurons, n_neurons))
 
     def train(self, patterns):
+
+        self.weights = np.zeros((self.n, self.n))
+
         for pattern in patterns:
             pattern = pattern.reshape(-1, 1)
             self.weights += np.dot(pattern, pattern.T)
+            # print(self.weights)
+
+        np.fill_diagonal(self.weights, 0)
+
+        print(self.weights)
+
+
     
 
 
 if __name__ == "__main__":
-    p1 = np.array([1, 1, -1, -1, 1, -1])
-    p2 = np.array([-1, 1, 1, -1, 1, -1])
+    p1 = np.array([1, 1, 1, -1, -1, -1])
+    p2 = np.array([-1, 1, -1, 1, -1, 1])
 
-    hopfield_network = HopfieldedNetwork(6)
-    hopfield_network.train([p1, p2])
+    net = HopfieldedNetwork(6)
+    net.train([p1, p2])
